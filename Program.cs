e663bb6f57414f1c1c24f0053a95ffa4df3f3658
KvaskovSS <sava0980@yandex.ru>
2022-12-09 19:21:41 +0300

    fix task 2

--- Program.cs
+++ Program.cs
@@ -13,10 +13,10 @@
 // }
 
 //Task 2
-// void distance(double[] dot1, double[] dot2){
-//     double distance = Math.Sqrt((Math.Pow(dot1[0] - dot2[0], 2)) + (Math.Pow(dot1[1] - dot2[1], 2)) + (Math.Pow(dot1[2] - dot2[2], 2)));
-//     Console.WriteLine($"Дистанция между точками = {distance}");
-// }
+void distance(double[] dot1, double[] dot2){
+    double distance = Math.Sqrt((Math.Pow(dot1[0] - dot2[0], 2)) + (Math.Pow(dot1[1] - dot2[1], 2)) + (Math.Pow(dot1[2] - dot2[2], 2)));
+    Console.WriteLine($"Дистанция между точками = {Math.Round(distance, 2)}");
+}
 
 //Task 3
 void square(int num){
@@ -38,21 +38,21 @@ void square(int num){
 
 
 //Task 2
-// Console.Write("Введите первую точку (x, y, z) : ");
-// double[] dot1 = new double[3];
-// for(int i = 0; i < dot1.Length; i++){
-//     Console.Write($"Введите {i + 1}  координату : ");
-//     dot1[i] =  Convert.ToDouble(Console.ReadLine());
-// }
-// Console.Write("Введите вторую точку (x, y, z) : ");
-// double[] dot2 = new double[3];
-// for(int i = 0; i < dot2.Length; i++){
-//     Console.Write($"Введите {i + 1}  координату : ");
-//     dot2[i] =  Convert.ToDouble(Console.ReadLine());
-// }
-// distance(dot1, dot2);
+Console.Write("Введите первую точку (x, y, z) : ");
+double[] dot1 = new double[3];
+for(int i = 0; i < dot1.Length; i++){
+    Console.Write($"Введите {i + 1}  координату : ");
+    dot1[i] =  Convert.ToDouble(Console.ReadLine());
+}
+Console.Write("Введите вторую точку (x, y, z) : ");
+double[] dot2 = new double[3];
+for(int i = 0; i < dot2.Length; i++){
+    Console.Write($"Введите {i + 1}  координату : ");
+    dot2[i] =  Convert.ToDouble(Console.ReadLine());
+}
+distance(dot1, dot2);
 
 //Task 3
-Console.Write("Введите  число : ");
-int num = Convert.ToInt32(Console.ReadLine());
-square(num);
+// Console.Write("Введите  число : ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// square(num);
